2c2672db466cc14c9b9d5198afd4f171f06075f6
Valoenu 👨🏻‍💻 <valoenu.contact@gmail.com>
2025-08-06 11:18:39 +0200

    Update PageNav.jsx

--- 11-worldwise/final/src/components/PageNav.jsx
+++ 11-worldwise/final/src/components/PageNav.jsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import styles from "./PageNav.module.css";
 
+'''Reusable component'''
+
 function PageNav() {
   return (
     <nav className={styles.nav}>
@@ -9,10 +11,10 @@ function PageNav() {
 
       <ul>
         <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink> // each link when homepage
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/product">Product</NavLink> 
         </li>
         <li>
           <NavLink to="/login" className={styles.ctaLink}>
