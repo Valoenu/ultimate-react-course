aab296e77b8e6725bf279d902747b330863ab0cb
Valoenu 👨🏻‍💻 <valoenu.contact@gmail.com>
2025-08-07 12:14:25 +0200

    Update FakeAuthContext.jsx

--- 11-worldwise/final/src/contexts/FakeAuthContext.jsx
+++ 11-worldwise/final/src/contexts/FakeAuthContext.jsx
@@ -1,22 +1,24 @@
 import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext(); // Create Authorization Context
 
-const initialState = {
+const initialState = { //user will be null at the beginning without login
   user: null,
   isAuthenticated: false,
 };
 
 function reducer(state, action) {
-  switch (action.type) {
-    case "login":
-      return { ...state, user: action.payload, isAuthenticated: true };
-    case "logout":
-      return { ...state, user: null, isAuthenticated: false };
-    default:
+  switch (action.type) { // There are only two actions
+    case "login": // Login action
+      return { ...state, user: action.payload, isAuthenticated: true }; // user will be assignment with user payload and set isAuthenticated to True.
+    case "logout": // Logout action
+      return { ...state, user: null, isAuthenticated: false }; //user when he will logout he will be back to null and set isAuthenticated to falls
+    default: // If error 
       throw new Error("Unknown action");
   }
 }
+
+// Never do this becouse everyone will get access to users data
 
 const FAKE_USER = {
   name: "Jack",
@@ -25,19 +27,21 @@ const FAKE_USER = {
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
 
-function AuthProvider({ children }) {
-  const [{ user, isAuthenticated }, dispatch] = useReducer(
+function AuthProvider({ children }) { // Will receive the children, new context ~ the same receipe,
+  const [{ user, isAuthenticated }, dispatch] = useReducer( // dispatch 
     reducer,
     initialState
   );
 
-  function login(email, password) {
-    if (email === FAKE_USER.email && password === FAKE_USER.password)
-      dispatch({ type: "login", payload: FAKE_USER });
+// We cant use dispatch becouse we will do something with Login
+
+  function login(email, password) { // Function that will need email and password
+    if (email === FAKE_USER.email && password === FAKE_USER.password) // Check if the user data is correct with the login
+      dispatch({ type: "login", payload: FAKE_USER }); // Then assign/dispatch action of login with the payload with the fake user
   }
 
-  function logout() {
-    dispatch({ type: "logout" });
+  function logout() { // doesn't need any information
+    dispatch({ type: "logout" }); // dispatch logout event
   }
 
   return (
@@ -47,11 +51,11 @@ function AuthProvider({ children }) {
   );
 }
 
-function useAuth() {
+function useAuth() { // function hook, biolerplate,
   const context = useContext(AuthContext);
-  if (context === undefined)
+  if (context === undefined) // check if the value is undefined, if it is then throw new error
     throw new Error("AuthContext was used outside AuthProvider");
-  return context;
+  return context; // otherwise just return context
 }
 
-export { AuthProvider, useAuth };
+export { AuthProvider, useAuth }; // export 
