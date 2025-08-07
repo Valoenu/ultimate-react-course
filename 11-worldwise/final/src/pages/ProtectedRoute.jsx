12de40208b7a2726edd0f69b460b9db46006bd54
Valoenu 👨🏻‍💻 <valoenu.contact@gmail.com>
2025-08-07 12:32:00 +0200

    Update ProtectedRoute.jsx

--- 11-worldwise/final/src/pages/ProtectedRoute.jsx
+++ 11-worldwise/final/src/pages/ProtectedRoute.jsx
@@ -2,18 +2,19 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
 
+// we will wrap the entire application, so you must use the children prop
 function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth(); // whether the user is authenticated or not to check
+  const navigate = useNavigate(); 
 
   useEffect(
     function () {
-      if (!isAuthenticated) navigate("/");
+      if (!isAuthenticated) navigate("/"); // Navigate to the other page, here becouse you cannot use navigate at the top level
     },
-    [isAuthenticated, navigate]
+    [isAuthenticated, navigate] // 
   );
 
   return isAuthenticated ? children : null;
 }
 
-export default ProtectedRoute;
+export default ProtectedRoute; // export
